ab67727c7d86b7c2b5b353e1a18426317e8b019d
thtgan <37025083+Thtgan@users.noreply.github.com>
2021-07-16 15:54:35 +0800

    Implement basic VGA text mode operation

--- src/kernel/drivers/vgaTextMode.c
+++ src/kernel/drivers/vgaTextMode.c
@@ -1,11 +1,12 @@
 #include"vgaTextMode.h"
 
-uint16_t cursorPosition;
+static uint16_t _cursorPosition;
+static uint8_t _vgaPattern;
 
 void initCursor()
 {
 	enableCursor(14, 15);
-	setCursorPosition(1, 0);
+	setCursorPosition(0, 0);
 }
 
 void enableCursor(uint8_t cursorBegin, uint8_t cursorEnd)
@@ -30,7 +31,7 @@ void setCursorPosition(uint8_t row, uint8_t col)
 
 static void __setCursorPosition(uint16_t position)
 {
-	cursorPosition = position;
+	_cursorPosition = position;
 	outb(0x03D4, 0x0F);
 	outb(0x03D5, position & 0xFF);
 	outb(0x03D4, 0x0E);
@@ -39,5 +40,101 @@ static void __setCursorPosition(uint16_t position)
 
 uint16_t getCursorPosition()
 {
-	return cursorPosition;
+	return _cursorPosition;
+}
+
+void setVgaCellPattern(uint8_t backgroundColor, uint8_t foregroundColor)
+{
+	_vgaPattern = VGA_COLOR_PATTERN(backgroundColor, foregroundColor);
+}
+
+void setDefaultVgaPattern()
+{
+	setVgaCellPattern(VGA_COLOR_BLUE, VGA_COLOR_WHITE);
+}
+
+static bool __putcharAtCursor(uint8_t ch)
+{
+	bool ret = false;
+	switch (ch)
+	{
+	case '\n':
+		__setCursorPosition((_cursorPosition / VGA_WIDTH + 1) * VGA_WIDTH);
+		break;
+	case '\r':
+		__setCursorPosition(_cursorPosition / VGA_WIDTH * VGA_WIDTH);
+		break;
+	default:
+		ret = true;
+		VGA_BUFFER_ADDRESS[_cursorPosition] = VGA_CELL_ENTRY(_vgaPattern, ch);
+		break;
+	}
+	return ret;
+}
+
+void putchar(uint8_t ch)
+{
+	if(__putcharAtCursor(ch))
+	{
+		__setCursorPosition(_cursorPosition + 1);
+	}
+}
+
+void print(const char* str)
+{
+	for(int i = 0; str[i] != '\0'; ++i)
+	{
+		putchar(str[i]);
+	}
+}
+
+const static char* _hexPrefix = "0x";
+const static char* _hexDigit = "0123456789ABCDEF";
+
+void printHex8(uint8_t val)
+{
+	print(_hexPrefix);
+	for(int i = 1; i >= 0; --i)
+		putchar(_hexDigit[(val >> (i << 2)) & 0x0F]);
+}
+
+void printHex16(uint16_t val)
+{
+	print(_hexPrefix);
+	for(int i = 3; i >= 0; --i)
+		putchar(_hexDigit[(val >> (i << 2)) & 0x0F]);
+}
+
+void printHex32(uint32_t val)
+{
+	print(_hexPrefix);
+	for(int i = 7; i >= 0; --i)
+		putchar(_hexDigit[(val >> (i << 2)) & 0x0F]);
+}
+
+void printHex64(uint64_t val)
+{
+	print(_hexPrefix);
+	for(int i = 15; i >= 0; --i)
+		putchar(_hexDigit[(val >> (i << 2)) & 0x0F]);
+}
+
+void printPtr(const void* ptr)
+{
+	printHex64((uint64_t)ptr);
+}
+
+void clearScreen()
+{
+	int limit = VGA_WIDTH * VGA_HEIGHT;
+	uint16_t cell = VGA_CELL_ENTRY(_vgaPattern, ' ');
+	for(int i = 0; i < limit; ++i)
+		VGA_BUFFER_ADDRESS[i] = cell;
+}
+
+void initVGA()
+{
+	setDefaultVgaPattern();
+	clearScreen();
+	initCursor();
 }
--- src/kernel/drivers/vgaTextMode.h
+++ src/kernel/drivers/vgaTextMode.h
@@ -2,38 +2,163 @@
 
 #include"portIO.h"
 #include<stdint.h>
+#include<stdbool.h>
 
-#define VGA_BUFFER_ADDRESS ((usigned char*)0xB800)
+#define VGA_BUFFER_ADDRESS ((uint16_t*)0xB8000)
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
 enum VGA_COLOR {
-    VGA_COLOR_BLACK         = 0,
-    VGA_COLOR_BLUE          = 1,
-    VGA_COLOR_GREEN         = 2,
-    VGA_COLOR_CYAN          = 3,
-    VGA_COLOR_RED           = 4,
-    VGA_COLOR_PURPLE        = 5,
-    VGA_COLOR_BROWN         = 6,
-    VGA_COLOR_GRAY          = 7,
-    VGA_COLOR_DARK_GRAY     = 8,
-    VGA_COLOR_LIGHT_BLUE    = 9,
-    VGA_COLOR_LIGHT_GREEN   = 10,
-    VGA_COLOR_LIGHT_CYAN    = 11,
-    VGA_COLOR_LIGHT_RED     = 12,
-    VGA_COLOR_LIGHT_PURPLE  = 13,
-    VGA_COLOR_YELLOW        = 14,
-    VGA_COLOR_WHITE         = 15
+    VGA_COLOR_BLACK         = 0,    //Black
+    VGA_COLOR_BLUE          = 1,    //Blue
+    VGA_COLOR_GREEN         = 2,    //Green
+    VGA_COLOR_CYAN          = 3,    //Cyan
+    VGA_COLOR_RED           = 4,    //Red
+    VGA_COLOR_PURPLE        = 5,    //Purple
+    VGA_COLOR_BROWN         = 6,    //Brown
+    VGA_COLOR_GRAY          = 7,    //Gray
+    VGA_COLOR_DARK_GRAY     = 8,    //Dark gray
+    VGA_COLOR_LIGHT_BLUE    = 9,    //Light blue
+    VGA_COLOR_LIGHT_GREEN   = 10,   //Light green
+    VGA_COLOR_LIGHT_CYAN    = 11,   //Light cyan
+    VGA_COLOR_LIGHT_RED     = 12,   //Light red
+    VGA_COLOR_LIGHT_PURPLE  = 13,   //Light purple
+    VGA_COLOR_YELLOW        = 14,   //Yellow
+    VGA_COLOR_WHITE         = 15    //White
 };
 
+#define VGA_BRIGHT_COLOR(x) ((x) | 8)
+#define VGA_COLOR_PATTERN(background, foreground) ((((background) & 0x0F) << 4) | ((foreground) & 0x0F))
+#define VGA_CELL_ENTRY(pattern, ch) (((((uint16_t)pattern) & 0xFF) << 8) | (ch & 0xFF))
+
+/**
+ * Enable the cursor and set its coverage on a character
+ * 
+ * The top row of a character has the index 0
+ * Both parameter should between (0, 15)
+ * And cursorBegin should not greater than cursorEnd
+ * 
+ * cursorBegin: The row where cursor begins to draw
+ * cursorEnd: The row where cursor ends drawing
+*/
 void enableCursor(uint8_t cursorBegin, uint8_t cursorEnd);
 
+/**
+ * Disable the cursor
+*/
 void disableCursor();
 
+/**
+ * Set the position of the cursor
+ * 
+ * row: The row index where cursor is, starts with 0, no greater than 24
+ * col: The col index where cursor is, starts with 0, no greater than 79
+*/
 void setCursorPosition(uint8_t row, uint8_t col);
 
+/**
+ * Set the position of the cursor
+ * 
+ * position: The distance of the position from first available position(0, 0) 
+*/
 static void __setCursorPosition(uint16_t position);
 
+/**
+ * Get the position of the cursor
+ * 
+ * return: The distance of the position from first available position(0, 0) 
+*/
 uint16_t getCursorPosition();
 
+/**
+ * Enable the cursor and set it position to (0, 0)
+ * Set it cover the last two row of the character
+*/
 void initCursor();
+
+/**
+ * Set the pattern of VGA cell
+ * All character printed then will use this pattern util new pattern is set
+ * 
+ * background: The background color of VGA cell
+ * foreground: The foreground color of VGA cell
+*/
+void setVgaCellPattern(uint8_t background, uint8_t foreground);
+
+/**
+ * Set to default VGA pattern
+ * Background: Blue
+ * Foreground: White
+*/
+void setDefaultVgaPattern();
+
+/**
+ * Put a character at the position of the cursor
+ * 
+ * ch: character to put
+ * 
+ * return: True if the sursor should move to next position
+*/
+static bool __putcharAtCursor(uint8_t ch);
+
+/**
+ * Put a character at current postion of the cursor
+ * If could, move the cursor to the next position
+ * 
+ * ch: character to put
+*/
+void putchar(uint8_t ch);
+
+/**
+ * Print the string, which should ends with '\0', start from the current position of cursor
+ * 
+ * str: string to print, ends with '\0'
+*/
+void print(const char* str);
+
+/**
+ * Print a 8-bit value in form of HEX, start from the current position of cursor
+ * 
+ * val: Value to print
+*/
+void printHex8(uint8_t val);
+
+/**
+ * Print a 16-bit value in form of HEX, start from the current position of cursor
+ * 
+ * val: Value to print
+*/
+void printHex16(uint16_t val);
+
+/**
+ * Print a 32-bit value in form of HEX, start from the current position of cursor
+ * 
+ * val: Value to print
+*/
+void printHex32(uint32_t val);
+
+/**
+ * Print a 64-bit value in form of HEX, start from the current position of cursor
+ * 
+ * val: Value to print
+*/
+void printHex64(uint64_t val);
+
+/**
+ * Print the value of the pointer, start from the current position of cursor
+ * 
+ * val: Pointer to print
+*/
+void printPtr(const void* ptr);
+
+/**
+ * Clear the screen with current pattern
+*/
+void clearScreen();
+
+/**
+ * Initialize VGA screen basically, including:
+ * Set Default pattern
+ * Initialize cursor
+*/
+void initVGA();
