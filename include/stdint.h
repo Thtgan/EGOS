8477f620acc2bcb6a081cd1c2be6c6fc211ea36c
Thtgan <37025083+Thtgan@users.noreply.github.com>
2021-11-19 01:57:53 +0800

    Tab fix

--- include/stdint.h
+++ include/stdint.h
@@ -1,10 +1,10 @@
 #if !defined(__STDINT_H)
 #define __STDINT_H
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
 
 typedef unsigned long uintptr_t;
 
