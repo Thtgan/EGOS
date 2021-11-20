53fa91fdcba451048aa9bc70cb02ea8facd6faa8
Thtgan <37025083+Thtgan@users.noreply.github.com>
2021-11-21 00:06:12 +0800

    Fix independent headers

--- include/stdarg.h
+++ include/stdarg.h
@@ -3,14 +3,11 @@
 
 #include<stdint.h>
 
-typedef uint8_t* va_list;
+typedef __builtin_va_list va_list;
 
-#define __SIZE_ROUND_UP(__TYPE) ((sizeof(__TYPE) + sizeof(uint8_t) - 1) / sizeof(uint8_t) * sizeof(uint8_t))
-
-#define va_start(__v, __l) (__v = (((va_list) &__l) + __SIZE_ROUND_UP(__l)))
-
-#define va_end(__v) //Do nothing
-
-#define va_arg(__v, __l) (__v += __SIZE_ROUND_UP(__l), *((__l *) (__v - __SIZE_ROUND_UP(__l))))
+#define va_start(__LIST, __LAST_ARG)    __builtin_va_start(__LIST, __LAST_ARG)
+#define va_end(__LIST)                  __builtin_va_end(__LIST)
+#define va_arg(__LIST, __TYPE)          __builtin_va_arg(__LIST, __TYPE)
+#define va_copy(__DST, __SRC)	        __builtin_va_copy(__DST, __SRC)
 
 #endif // __STDARG_H
--- include/stdbool.h
+++ include/stdbool.h
@@ -1,8 +1,8 @@
 #if !defined(__STDBOOL_H)
 #define __STDBOOL_H
 
-#define bool _Bool
-#define true 1
-#define false 0
+#define bool    _Bool
+#define true    1
+#define false   0
 
 #endif // __STDBOOL_H
