fbb3340bf3ed7da5a7030e04c7c316d3e9f57f34
Thtgan <37025083+Thtgan@users.noreply.github.com>
2023-02-09 16:17:36 +0800

    page allocator rework

--- kernel/memory/pageAlloc.c
+++ kernel/memory/pageAlloc.c
@@ -8,40 +8,7 @@
 #include<system/memoryMap.h>
 #include<system/pageTable.h>
 
-typedef struct {
-    PageNodeList freePageNodeList;  //Linker list formed by allocatable page nodes, with head node
-    void* freePageBase;             //Beginning pointer of the allocatable pages(Direct access pointer)
-    size_t freePageSize;            //How many free pages this pool has
-} _PagePool;
-
-static _PagePool _pool;
-
-/**
- * @brief Initialize the paging pool
- * 
- * @param p Paging pool
- * @param areaBegin Beginning of the available memory area
- * @param areaPageSize The num of available pages contained by area
- */
-static void __initPagePool(_PagePool* p, void* areaBegin, size_t areaPageSize);
-
-/**
- * @brief Return a serial of unused continued pages from pool, and set them to used
- * 
- * @param p Pool
- * @param n The num of pages to allocate
- * @return void* The beginning of the pages, NULL if no satisified pages available;
- */
-static void* __poolAllocatePages(_PagePool* p, size_t n);
-
-/**
- * @brief Release a serial of continued pages back to pool, and set them to unused
- * 
- * @param p Pool
- * @param pageBegin The beginning of pages
- * @param n The num of continued pages to release
- */
-static void __poolReleasePages(_PagePool* p, void* pagesBegin, size_t n);
+static PageNodeList _freePageNodeList;
 
 /**
  * @brief Collect the page node which was cut out from the list
@@ -49,16 +16,18 @@ static void __poolReleasePages(_PagePool* p, void* pagesBegin, size_t n);
  * @param list Page node list, where the pages to collect came from
  * @param node Page node contiains pages to collect
  */
-static void __collectPages(_PagePool* p, PageNode* node);
+static void __collectPages(PageNodeList* list, PageNode* node);
 
 void initPageAlloc() {
     MemoryMap* mMap = (MemoryMap*)sysInfo->memoryMap;
 
-    __initPagePool(&_pool, (void*)((uintptr_t)mMap->freePageBegin << PAGE_SIZE_SHIFT), mMap->freePageEnd - mMap->freePageBegin);
+    initPageNodeList(&_freePageNodeList, (void*)((uintptr_t)mMap->freePageBegin << PAGE_SIZE_SHIFT), mMap->freePageEnd - mMap->freePageBegin); //Initialize the page node list
 }
 
 void* pageAlloc(size_t n, PhysicalPageType type) {
-    void* ret = __poolAllocatePages(&_pool, n);
+    PageNode* ret = firstFitFindPages(&_freePageNodeList, n);
+    cutPageNodeFront(ret, n);
+    
     if (ret == NULL) {
         return NULL;
     }
@@ -72,7 +41,9 @@ void* pageAlloc(size_t n, PhysicalPageType type) {
 }
 
 void pageFree(void* pPageBegin, size_t n) {
-    __poolReleasePages(&_pool, pPageBegin, n);
+    PageNode* newNode = initPageNode(pPageBegin, n);
+    __collectPages(&_freePageNodeList, newNode);
+
     PhysicalPage* physicalPageStruct = getPhysicalPageStruct(pPageBegin);
     for (int i = 0; i < n; ++i) {
         (physicalPageStruct + i)->flags = PHYSICAL_PAGE_TYPE_NORMAL;
@@ -80,39 +51,16 @@ void pageFree(void* pPageBegin, size_t n) {
     }
 }
 
-static void __initPagePool(_PagePool* p, void* areaBegin, size_t areaPageSize) {
-    p->freePageBase = areaBegin;
-    p->freePageSize = areaPageSize;
-    
-    initPageNodeList(&p->freePageNodeList, p->freePageBase, p->freePageSize); //Initialize the page node list
-}
-
-static void* __poolAllocatePages(_PagePool* p, size_t n) {
-    PageNode* node = firstFitFindPages(&p->freePageNodeList, n);
-
-    void* ret = node->base;
-
-    cutPageNodeFront(node, n);
-
-    return ret;
-}
-
-static void __poolReleasePages(_PagePool* p, void* pagesBegin, size_t n) {
-    PageNode* newNode = initPageNode(pagesBegin, n);
-    __collectPages(p, newNode);
-}
-
-static void __collectPages(_PagePool* p, PageNode* node) {
-    PageNodeList* list = &p->freePageNodeList;
+static void __collectPages(PageNodeList* list, PageNode* node) {
     PageNode* position = HOST_POINTER(list->next, PageNode, node);
     
-    if (node->base < position->base) {  //If node to collect has the lowest base
+    if ((void*)node < (void*)position) {  //If node to collect has the lowest base
         insertPageNodeFront(position, node);
     } else {
         //Find a node whose base is higher than the node to collect (Theoreticlly, equal is impossible)
         while (true) {
             LinkedListNode* nextListNode = linkedListGetNext(&position->node);
-            if (nextListNode == list || HOST_POINTER(nextListNode, PageNode, node)->base > node->base) {
+            if (nextListNode == list || (void*)HOST_POINTER(nextListNode, PageNode, node) > (void*)node) {
                 insertPageNodeBack(position, node);
                 break;
             } else {
@@ -128,14 +76,14 @@ static void __collectPages(_PagePool* p, PageNode* node) {
     PageNode* combinedNode = node;
     if (prevListNode != list) {
         PageNode* prevNode = HOST_POINTER(prevListNode, PageNode, node);
-        if (prevNode->base + (prevNode->length << PAGE_SIZE_SHIFT) == combinedNode->base) {
+        if ((void*)prevNode + (prevNode->length << PAGE_SIZE_SHIFT) == (void*)combinedNode) {
             combinedNode = combinePrevPageNode(combinedNode);
         }
     }
 
     if (nextListNode != list) {
         PageNode* nextNode = HOST_POINTER(nextListNode, PageNode, node);
-        if (combinedNode->base + (combinedNode->length << PAGE_SIZE_SHIFT) == nextNode->base) {
+        if ((void*)combinedNode + (combinedNode->length << PAGE_SIZE_SHIFT) == (void*)nextNode) {
             combinedNode = combineNextPageNode(combinedNode);
         }
     }
