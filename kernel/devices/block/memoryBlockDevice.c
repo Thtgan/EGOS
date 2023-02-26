6fb733f6a7ed8ba584a077e31df2a03b611b6ccc
Thtgan <37025083+Thtgan@users.noreply.github.com>
2023-02-27 03:46:40 +0800

    Fix again

--- kernel/devices/block/memoryBlockDevice.c
+++ kernel/devices/block/memoryBlockDevice.c
@@ -11,9 +11,9 @@
 
 static bool _created = false;
 
-static void __readBlocks(BlockDevice* this, block_index_t blockIndex, void* buffer, size_t n);
+static int __readBlocks(BlockDevice* this, block_index_t blockIndex, void* buffer, size_t n);
 
-static void __writeBlocks(BlockDevice* this, block_index_t blockIndex, const void* buffer, size_t n);
+static int __writeBlocks(BlockDevice* this, block_index_t blockIndex, const void* buffer, size_t n);
 
 static BlockDeviceOperation _operations = {
     .readBlocks = __readBlocks,
@@ -43,10 +43,12 @@ void deleteMemoryBlockDevice(BlockDevice* blockDevice) {
     _created = false;
 }
 
-static void __readBlocks(BlockDevice* this, block_index_t blockIndex, void* buffer, size_t n) {
+static int __readBlocks(BlockDevice* this, block_index_t blockIndex, void* buffer, size_t n) {
     memcpy(buffer, (void*)this->additionalData + blockIndex * BLOCK_SIZE, n * BLOCK_SIZE);  //Just simple memcpy
+    return 0;
 }
 
-static void __writeBlocks(BlockDevice* this, block_index_t blockIndex, const void* buffer, size_t n) {
+static int __writeBlocks(BlockDevice* this, block_index_t blockIndex, const void* buffer, size_t n) {
     memcpy((void*)this->additionalData + blockIndex * BLOCK_SIZE, buffer, n * BLOCK_SIZE);  //Just simple memcpy
+    return 0;
 }
