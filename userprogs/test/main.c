3c1f150bb053bcad8b83dc02097b732ac86d70fa
Thtgan <37025083+Thtgan@users.noreply.github.com>
2025-11-28 22:47:59 +0800

    User program fix

--- userprogs/test/main.c
+++ userprogs/test/main.c
@@ -6,7 +6,7 @@ int main(int argc, const char *argv[]) {
     initFunc();
 
     if (func(9) - 114514 == 9) {
-        syscall3(SYSCALL_WRITE, 0, (uint64_t)"THIS IS A TEST LINE FROM USER PROG\n", 35);
+        syscall3(SYSCALL_WRITE, 1, (uint64_t)"THIS IS A TEST LINE FROM USER PROG\n", 35);   //TODO: Write to stdin crashes the kernel
         syscall6(SYSCALL_TEST, 0, 1, 2, 3, 4, argc);
     }
 
